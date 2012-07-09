5f1cd46fb87f751f4160a76011e097676a95340a
Dan Stocker <dan@kwaia.com>
2012-07-10 00:12:33 +0200

    code clarity improvement in rjson.js

--- js/rjson.js
+++ js/rjson.js
@@ -8,64 +8,69 @@ var fs = require('fs'),
     troop = require('../lib/troop/troop-0.1.3').troop,
     rjson;
 
-rjson = troop.base.extend()
-    .addMethod({
-        /**
-         * Initializes RJSON.
-         * @param fileName {string} Name of database file.
-         */
-        init: function (fileName) {
-            this.fileName = fileName;
-        },
+rjson = troop.base.extend({
+    //////////////////////////////
+    // OOP
 
-        /**
-         * Reads the whole RJSON database file.
-         * @param handler {function} Callback
-         */
-        read: function (handler) {
-            fs.readFile(this.fileName, function (err, data) {
-                var parsed;
+    /**
+     * Initializes RJSON.
+     * @param fileName {string} Name of database file.
+     */
+    init: function (fileName) {
+        this.fileName = fileName;
+    },
+
+    //////////////////////////////
+    // Datastore methods
+
+    /**
+     * Reads the whole RJSON database file.
+     * @param handler {function} Callback
+     */
+    read: function (handler) {
+        fs.readFile(this.fileName, function (err, data) {
+            var parsed;
+            if (typeof handler === 'function') {
+                if (!err) {
+                    try {
+                        parsed = JSON.parse('{' + data.toString().slice(0, -1) + '}');
+                    } catch (e) {
+                        err = new Error("Corrupted database contents.");
+                    }
+                }
+                handler(err, parsed);
+            }
+        });
+        return this;
+    },
+
+    /**
+     * Compacts database files.
+     * @param handler {function} Callback
+     */
+    compact: function (handler) {
+        var that = this;
+        this.read(function (err, data) {
+            if (err) {
                 if (typeof handler === 'function') {
-                    if (!err) {
-                        try {
-                            parsed  = JSON.parse('{' + data.toString().slice(0, -1) + '}');
-                        } catch (e) {
-                            err = new Error("Corrupted database contents.");
-                        }
-                    }
-                    handler(err, parsed);
+                    handler(err);
                 }
-            });
-            return this;
-        },
+            } else {
+                fs.writeFile(that.fileName, JSON.stringify(data).slice(1, -1) + ',', handler);
+            }
+        });
+        return this;
+    },
 
-        /**
-         * Compacts database files.
-         * @param handler {function} Callback
-         */
-        compact: function (handler) {
-            var that = this;
-            this.read(function (err, data) {
-                if (err) {
-                    if (typeof handler === 'function') {
-                        handler(err);
-                    }
-                } else {
-                    fs.writeFile(that.fileName, JSON.stringify(data).slice(1, -1) + ',', handler);
-                }
-            });
-            return this;
-        },
-
-        /**
-         * Writes object to database.
-         * @param data {object}
-         * @param handler {function} Callback
-         */
-        write: function (data, handler) {
-            fs.appendFile(this.fileName, JSON.stringify(data).slice(1, -1) + ',', handler);
-            return this;
-        }
-    });
+    /**
+     * Writes object to database.
+     * @param data {object}
+     * @param handler {function} Callback
+     */
+    write: function (data, handler) {
+        fs.appendFile(this.fileName, JSON.stringify(data).slice(1, -1) + ',', handler);
+        return this;
+    }
+});
 
 exports.rjson = rjson;
