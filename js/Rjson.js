81be74945d7533c783b530bdf0c40c3623b0a3a6
Dan Stocker <dan@kwaia.com>
2012-12-30 17:48:30 +0100

    assertions for Rjson

--- js/Rjson.js
+++ js/Rjson.js
@@ -4,7 +4,7 @@
  * RJSON may contain the same key several times over. Upon parsing,
  * the last value (which is also the latest) will be used.
  */
-(function (fs, troop) {
+(function (fs, dessert, troop) {
     var self = exports.Rjson = troop.Base.extend()
         .addMethod({
             //////////////////////////////
@@ -26,29 +26,36 @@
              * @param handler {function} Callback
              */
             read: function (handler) {
+                dessert.isFunction(handler);
+
                 var that = this;
+
                 fs.readFile(that.fileName, function (err, data) {
                     var parsed;
-                    if (typeof handler === 'function') {
-                        if (!err) {
-                            try {
-                                parsed = JSON.parse('{' + data.toString().slice(0, -1) + '}');
-                            } catch (e) {
-                                err = new Error("Corrupted database contents.");
-                            }
+
+                    if (!err) {
+                        try {
+                            parsed = JSON.parse('{' + data.toString().slice(0, -1) + '}');
+                        } catch (e) {
+                            err = new Error("Corrupted database contents.");
                         }
-                        handler.call(that, err, parsed);
                     }
+
+                    handler.call(that, err, parsed);
                 });
+
                 return this;
             },
 
             /**
              * Compacts database files.
-             * @param handler {function} Callback
+             * @param [handler] {function} Callback
              */
             compact: function (handler) {
+                dessert.isFunctionOptional(handler);
+
                 var that = this;
+
                 this.read(function (err, data) {
                     if (err) {
                         if (typeof handler === 'function') {
@@ -62,25 +69,33 @@
                         });
                     }
                 });
+
                 return this;
             },
 
             /**
              * Writes object to database.
              * @param data {object}
-             * @param handler {function} Callback
+             * @param [handler] {function} Callback
              */
             write: function (data, handler) {
+                dessert
+                    .isObject(data)
+                    .isFunctionOptional(handler);
+
                 var that = this;
+
                 fs.appendFile(that.fileName, JSON.stringify(data).slice(1, -1) + ',', function () {
                     if (typeof handler === 'function') {
                         handler.apply(that, arguments);
                     }
                 });
+
                 return that;
             }
         });
 }(
     require('fs'),
+    require('dessert-0.2.1').dessert,
     require('troop-0.2.1').troop
 ));
