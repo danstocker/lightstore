3882615d0842a7908c20a0bcfa605743ea538ce5
Dan Stocker <dan@kwaia.com>
2013-01-28 00:17:02 +0100

    simple mock require with troop

--- js/mocks.js
+++ js/mocks.js
@@ -1,16 +1,4 @@
 /*global troop */
 (function () {
-    // registry of fake modules
-    var modules = {};
-
-    this.require = function (moduleName) {
-        var module;
-        if (modules.hasOwnProperty(moduleName)) {
-            return modules[moduleName];
-        } else {
-            module = troop.Base.extend();
-            modules[moduleName] = module;
-            return module;
-        }
-    };
+    this.require = troop.mockRequire();
 }());
